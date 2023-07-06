e3585650841d65b6abf391a51962d114dfa67c0e
AllanSantos-DV <allannascimentodossantos@gmail.com>
2023-07-06 11:40:10 -0300

    Alterado a logica do exercicio 4 para abordar as pontuações ('.', '?', '!', ':')

--- script.js
+++ script.js
@@ -5,34 +5,30 @@ function validarEntrada(valor) {
   }
   
   function reverterPalavras() {
-    var inputValue = document.getElementById("input1").value;
+    const inputValue = document.getElementById("input1").value;
     validarEntrada(inputValue);
-  
-    var palavras = inputValue.split(" ");
-    var resultado = palavras.reverse().join(" ");
-  
-    document.getElementById("resultado1").textContent = resultado;
+
+    const palavras = inputValue.split(" ");
+    document.getElementById("resultado1").textContent = palavras.reverse().join(" ");
   }
   
   function removerDuplicados() {
-    var inputValue = document.getElementById("input2").value;
+    const inputValue = document.getElementById("input2").value;
     validarEntrada(inputValue);
-  
-    var caracteres = inputValue.split("");
-    var caracteresUnicos = [...new Set(caracteres)];
-    var resultado = caracteresUnicos.join("");
-  
-    document.getElementById("resultado2").textContent = resultado;
+
+    const caracteres = inputValue.split("");
+    const caracteresUnicos = [...new Set(caracteres)];
+    document.getElementById("resultado2").textContent = caracteresUnicos.join("");
   }
   
   function encontrarMaiorPalindromo() {
-    var inputValue = document.getElementById("input3").value;
+    const inputValue = document.getElementById("input3").value;
     validarEntrada(inputValue);
   
     function isPalindromo(string) {
-      var left = 0;
-      var right = string.length - 1;
-  
+      let left = 0;
+      let right = string.length - 1;
+
       while (left < right) {
         if (string.charAt(left) !== string.charAt(right)) {
           return false;
@@ -43,11 +39,11 @@ function validarEntrada(valor) {
   
       return true;
     }
-  
-    var maiorPalindromo = "";
-    for (var i = 0; i < inputValue.length; i++) {
-      for (var j = i + 1; j <= inputValue.length; j++) {
-        var substring = inputValue.substring(i, j);
+
+    let maiorPalindromo = "";
+    for (let i = 0; i < inputValue.length; i++) {
+      for (let j = i + 1; j <= inputValue.length; j++) {
+        const substring = inputValue.substring(i, j);
         if (isPalindromo(substring) && substring.length > maiorPalindromo.length) {
           maiorPalindromo = substring;
         }
@@ -56,34 +52,36 @@ function validarEntrada(valor) {
   
     document.getElementById("resultado3").textContent = maiorPalindromo;
   }
-  
-  function capitalizarFrase() {
-    var inputValue = document.getElementById("input4").value;
-    validarEntrada(inputValue);
-  
-    var frases = inputValue.split(/(?<=\.|\?)\s*/);
-    var resultado = frases
-      .map(function (frase) {
-        return frase.charAt(0).toUpperCase() + frase.slice(1);
-      })
-      .join(" ");
-  
-    document.getElementById("resultado4").textContent = resultado;
-  }
-  
+
+function formatarFrase() {
+  const pontuacoesConhecidas = ['.', '!', '?', ':'];
+  const fraseElement = document.getElementById("input4").value;
+
+  const palavras = fraseElement.split(' ');
+  const novaFrase = palavras.map((palavra, i) => {
+    if (i === 0 || pontuacoesConhecidas.includes(palavras[i - 1].slice(-1))) {
+      return palavra.charAt(0).toUpperCase() + palavra.slice(1);
+    } else {
+      return palavra;
+    }
+  });
+  document.getElementById('resultado4').innerText = novaFrase.join(' ');
+}
+
+
   function verificarAnagramaPalindromo() {
-    var inputValue = document.getElementById("input5").value;
+    const inputValue = document.getElementById("input5").value;
     validarEntrada(inputValue);
   
     function isAnagramaPalindromo(string) {
-      var charFrequencyMap = {};
-      for (var i = 0; i < string.length; i++) {
-        var char = string[i];
+      const charFrequencyMap = {};
+      for (let i = 0; i < string.length; i++) {
+        const char = string[i];
         charFrequencyMap[char] = (charFrequencyMap[char] || 0) + 1;
       }
-  
-      var oddCount = 0;
-      for (var count of Object.values(charFrequencyMap)) {
+
+      let oddCount = 0;
+      for (const count of Object.values(charFrequencyMap)) {
         if (count % 2 !== 0) {
           oddCount++;
         }
@@ -91,8 +89,7 @@ function validarEntrada(valor) {
   
       return oddCount <= 1;
     }
-  
-    var resultado = isAnagramaPalindromo(inputValue) ? "true" : "false";
-    document.getElementById("resultado5").textContent = resultado;
+
+  document.getElementById("resultado5").textContent = isAnagramaPalindromo(inputValue) ? "true" : "false";
   }
   
